cbdadb283a8a5058f255d5a9c59afd431486d7a1
wolf-by <wolf.by@yandex.ru>
2023-09-07 16:01:29 +0300

    Add Clients migration

---
+++ db/migrate/20230907125123_name_of_migration.rb
@@ -0,0 +1,13 @@
+class NameOfMigration < ActiveRecord::Migration[7.0]
+  def change
+    create_table :client do |t|
+      t.text :name
+      t.text :phone
+      t.text :datestamp
+      t.text :barber
+      t.text :color
+
+      t.timestamps
+    end  
+  end
+end
