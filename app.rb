328543eb84f488d2e5c53fa614d81ba59e868e29
wolf-by <wolf.by@yandex.ru>
2023-09-12 15:11:47 +0300

    Add Barbers migration

--- app.rb
+++ app.rb
@@ -4,11 +4,13 @@ require 'sinatra'
 require 'sinatra/reloader'
 require "sinatra/activerecord"
 
-set :database, "sqlite3:barbershop.db"
+set :database, {adapter: "sqlite3", database: "barbershop.db"}
 
 class Client < ActiveRecord::Base
-
 end		
+
+class Barber < ActiveRecord::Base
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
